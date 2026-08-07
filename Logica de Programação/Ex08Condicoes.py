f77c6cf054f69b6d76f6047f316a8baf09484d73
Scooby <Cain Haniel>
2026-08-07 16:18:00 -0300

    Exercicio de Condicoes

--- Logica de Programação/Ex08Condicoes.py
+++ Logica de Programação/Ex08Condicoes.py
@@ -1,2 +1,12 @@
 #exercicio de condição entrada e saida
 #Se o usuario digitar Entrar EnTrar ou ENTRAR, o sistema tem que aceitar normalmente. 
+
+entrada = input('Digite "ENTRAR", para entrar no sistema e "SAIR" para sair do sistema: ')
+letra_maiuscula = entrada.upper()
+
+if letra_maiuscula == "ENTRAR":
+    print('Seja bem vindo ao sistema.')
+elif letra_maiuscula == "SAIR":
+    print('Volte sempre.')
+else:
+    print('Você digitou errado.')
