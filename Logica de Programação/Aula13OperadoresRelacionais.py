9d202b68cb54383308810f24721186804d3bb753
Scooby <Cain Haniel>
2026-08-07 17:10:14 -0300

    Operadores Relacionais

---
+++ Logica de Programação/Aula13OperadoresRelacionais.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (Relacionais)
+OP          Significado         Exemplo (True)
+>           maior               2 > 1
+>=          maior igual         2 >= 1
+<           menor               2 < 1
+<           menor  igual        2 <= 1
+<=          menor  igual        2 <= 1
+==          igual              'a' == 'a'
+!=('! =)    diferente          'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 1
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
