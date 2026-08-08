33d3e1d56856e02815178a56f356becbbfe281ad
Scooby <Cain Haniel>
2026-08-08 15:35:20 -0300

    Interpolação basica de String

---
+++ Logica de Programação/Aula15Interpolacao_String.py
@@ -0,0 +1,11 @@
+"""
+Interpolação básica de String
+s - string
+d e i - int
+f - float
+x e x - Hexadecimal (ABCDEF0123456789)
+"""
+nome = 'Caina'
+preco = 100.233
+variavel = '%s, o preco total foi R$%f' % (nome, preco)
+print(variavel)
